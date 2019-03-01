45cc233e8b50f8c299662203ce9f0eb603eaa414
Florian Wörister <ffwoerister@gmail.com>
2019-03-01 23:57:09 +0100

    don't look for latest branch in build script

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,9 +10,12 @@ sudo apt-get install solr-jetty
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
-echo "CKAN branch: $latest_ckan_release_branch"
-git checkout $latest_ckan_release_branch
+# export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
+# echo "CKAN branch: $latest_ckan_release_branch"
+# git checkout $latest_ckan_release_branch
+
+echo "skip looking for latest branch"
+
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
