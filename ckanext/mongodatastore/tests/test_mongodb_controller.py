c96b88a1f08060829a0b49e706e9ef45a6004c3f
Florian Wörister <ffwoerister@gmail.com>
2019-03-02 18:26:50 +0100

    mongodb controller tests added

--- ckanext/mongodatastore/tests/test_mongodb_controller.py
+++ ckanext/mongodatastore/tests/test_mongodb_controller.py
@@ -15,6 +15,9 @@ TEST_RESULT_CSV = "1;Florian;12\r\n2;Michael;13\r\n"
 
 
 class MongoDbControllerTest(unittest.TestCase):
+
+    # helper function tests
+
     def test_convert_to_csv(self):
         result = str(convert_to_csv(TEST_RESULT_SET, TEST_RESULT_SET_KEYS))
         self.assertEqual(result, TEST_RESULT_CSV)
@@ -29,8 +32,36 @@ class MongoDbControllerTest(unittest.TestCase):
         unix_timestamp = convert_to_unix_timestamp(datetime_value)
         self.assertEqual(unix_timestamp, 0)
 
+    # static function tests
+
     def singleton_test(self):
         first_instance = MongoDbController.getInstance()
         second_instance = MongoDbController.getInstance()
 
         self.assertEqual(first_instance, second_instance)
+
+    def reaload_config_test(self):
+        new_cfg = {
+            'ckan.datastore.write_url': 'localhost:27017',
+            'ckan.querystore.url': 'postgresql://query_store:query_store@localhost/query_store',
+            'ckan.datastore.search.rows_max': 120
+
+        }
+
+        first_instance = MongoDbController.getInstance()
+        MongoDbController.reloadConfig(new_cfg)
+        second_instance = MongoDbController.getInstance()
+
+        self.assertNotEqual(first_instance, second_instance)
+
+    # member function tests
+
+    def create_resource_test(self):
+        mongo_cntr = MongoDbController.getInstance()
+
+        new_resource_id = 'new_resource'
+
+        mongo_cntr.create_resource(new_resource_id)
+        all_ids = mongo_cntr.get_all_ids()
+
+        self.assertIn(new_resource_id, all_ids)
