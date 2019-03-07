471b069fd5bdafbb87e3d576ed96860149947a05
Florian Wörister <ffwoerister@gmail.com>
2019-03-08 00:01:48 +0100

    test if test fails because of race condition

--- ckanext/mongodatastore/tests/test_mongodb_controller.py
+++ ckanext/mongodatastore/tests/test_mongodb_controller.py
@@ -224,6 +224,8 @@ class MongoDbControllerTest(unittest.TestCase):
 
         mongo_cntr.upsert(new_resource_id, new_records, False)
 
+        sleep(4)
+
         result = mongo_cntr.query_current_state(new_resource_id, {}, {u'_id': 0, u'id': 1, u'field1': 1, u'field2': 1},
                                                 None, None, None, False, True)
 
